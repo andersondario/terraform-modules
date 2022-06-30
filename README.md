e27d4bfa3b4efb4a1ddeb7f22375e9f96ff7e098
andersondario <andersondario@gmail.com>
2022-06-30 08:44:04 -0300

    Changing Query and adding readme

---
+++ README.md
@@ -0,0 +1,2 @@
+# Terraform Sample
+This is a basic sample project created for test the Terraform Cloud capabilities.
--- modules/aws/ec2/main.tf
+++ modules/aws/ec2/main.tf
@@ -4,7 +4,7 @@ data "aws_ami" "amazon_linux" {
 
     filter {
         name          = "name"
-        values        = [ "Amazon Linux 2" ]
+        values        = [ "amzn-ami-hvm-*-x86_64-gp2" ]
     }
 
     filter {
