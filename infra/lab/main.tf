8be57ae0b4b0910ae7eb2ad55dc1ed98f0a90286
andersondario <andersondario@gmail.com>
2022-06-30 08:36:45 -0300

    Second

--- infra/lab/main.tf
+++ infra/lab/main.tf
@@ -5,6 +5,13 @@ terraform {
             version = "4.19.0"
         }
     }
+
+    backend "remote" {
+		organization = "andersondario"
+		workspaces {
+			name = "lab" 
+		}
+	}
 }
 
 provider "aws" {
