f4000677bbfae72a65d9edc774c228f2b191e0ea
andersondarioo <andersondario@gmail.com>
2022-07-19 16:10:23 -0300

    Refactor ecs module

--- modules/aws/ecs/main.tf
+++ modules/aws/ecs/main.tf
@@ -4,6 +4,10 @@ data "aws_availability_zones" "available_zones" {
 
 resource "aws_vpc" "default" {
   cidr_block = "10.32.0.0/16"
+
+  tags = {
+    "Name" = "${var.cluster_name}-vpc"
+  }
 }
 
 resource "aws_subnet" "public" {
@@ -60,7 +64,7 @@ resource "aws_route_table_association" "private" {
 }
 
 resource "aws_security_group" "lb" {
-  name        = "default-alb-security-group"
+  name        = "${var.app_name}-alb-security-group"
   vpc_id      = aws_vpc.default.id
 
   ingress {
@@ -79,32 +83,32 @@ resource "aws_security_group" "lb" {
 }
 
 resource "aws_lb" "default" {
-  name            = "example-lb"
+  name            = "${var.cluster_name}-lb"
   subnets         = aws_subnet.public.*.id
   security_groups = [aws_security_group.lb.id]
 }
 
-resource "aws_lb_target_group" "dummy_app" {
-  name        = "example-target-group"
+resource "aws_lb_target_group" "app" {
+  name        = "${var.app_name}-target-group"
   port        = 80
   protocol    = "HTTP"
   vpc_id      = aws_vpc.default.id
   target_type = "ip"
 }
 
-resource "aws_lb_listener" "dummy_app" {
+resource "aws_lb_listener" "app" {
   load_balancer_arn = aws_lb.default.id
   port              = "80"
   protocol          = "HTTP"
 
   default_action {
-    target_group_arn = aws_lb_target_group.dummy_app.id
+    target_group_arn = aws_lb_target_group.app.id
     type             = "forward"
   }
 }
 
-resource "aws_ecs_task_definition" "dummy_app" {
-  family                   = "dummy-app"
+resource "aws_ecs_task_definition" "app" {
+  family                   = var.app_name
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = 256
@@ -114,8 +118,8 @@ resource "aws_ecs_task_definition" "dummy_app" {
   container_definitions = <<DEFINITION
 [
   {
-      "name": "dummy-app",
-      "image": "167947257750.dkr.ecr.us-east-1.amazonaws.com/dummy-app",
+      "name": "${var.app_name}",
+      "image": "167947257750.dkr.ecr.us-east-1.amazonaws.com/${var.app_name}",
       "memory": 256,
       "cpu": 256,
       "essential": true,
@@ -130,7 +134,7 @@ resource "aws_ecs_task_definition" "dummy_app" {
         "logDriver": "awslogs",
         "secretOptions": null,
         "options": {
-          "awslogs-group": "/ecs/dummy-app",
+          "awslogs-group": "/ecs/${var.app_name}",
           "awslogs-region": "us-east-1",
           "awslogs-stream-prefix": "ecs"
         }
@@ -140,8 +144,8 @@ resource "aws_ecs_task_definition" "dummy_app" {
 DEFINITION
 }
 
-resource "aws_security_group" "dummy_app_task" {
-  name        = "example-task-security-group"
+resource "aws_security_group" "app_task" {
+  name        = "${var.app_name}-task-security-group"
   vpc_id      = aws_vpc.default.id
 
   ingress {
@@ -163,25 +167,25 @@ resource "aws_ecs_cluster" "main" {
   name = var.cluster_name
 }
 
-resource "aws_ecs_service" "dummy_app" {
-  name            = "dummy-app-service"
+resource "aws_ecs_service" "app" {
+  name            = "${var.app_name}-service"
   cluster         = aws_ecs_cluster.main.id
-  task_definition = aws_ecs_task_definition.dummy_app.arn
+  task_definition = aws_ecs_task_definition.app.arn
   desired_count   = var.app_count
   launch_type     = "FARGATE"
 
   network_configuration {
-    security_groups = [aws_security_group.dummy_app_task.id]
+    security_groups = [aws_security_group.app_task.id]
     subnets         = aws_subnet.private.*.id
   }
 
   load_balancer {
-    target_group_arn = aws_lb_target_group.dummy_app.id
-    container_name   = "dummy-app"
+    target_group_arn = aws_lb_target_group.app.id
+    container_name   = var.app_name
     container_port   = 3000
   }
 
-  depends_on = [aws_lb_listener.dummy_app]
+  depends_on = [aws_lb_listener.app]
 }
 
 output "load_balancer_ip" {
