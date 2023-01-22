9db3f13f30e08e1aa0332e2b0a01edda011b85fa
andersondario <andersondario@gmail.com>
2023-01-22 14:31:37 -0300

    Fixing syntax

--- modules/gcp/external-ip/main.tf
+++ modules/gcp/external-ip/main.tf
@@ -1,12 +1,5 @@
 resource "google_compute_address" "static" {
   name         = var.ip_name
-  address_type = var.type
-  ip_version   = "IPV4"
-  project      = var.project
-}
-
-resource "google_compute_address" "internal_with_subnet_and_address" {
-  name         = var.ip_name
   subnetwork   = var.subnet_name
   address_type = "EXTERNAL"
   region       = var.region
